4badd2c6e6d1844ca091ad14b3fe8368fe985c61
Alexandr Logvinov <ato31ato@gmail.com>
2024-01-07 21:32:56 +0200

    fix for signalR authentication

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -66,6 +66,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 // security key validation
                 ValidateIssuerSigningKey = true,
             };
+
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    var accessToken = context.Request.Query["access_token"];
+
+                    var path = context.HttpContext.Request.Path;
+                    if (!string.IsNullOrEmpty(accessToken) &&
+                        (path.StartsWithSegments("/hubs/chat")))
+                    {
+                        context.Token = accessToken;
+                    }
+                    return Task.CompletedTask;
+                }
+            };
         });
 
 
